3752a7e0aee6bc3bdec4b426a24c1aab07e72d80
klknn <klknn.gh@gmail.com>
2025-11-19 22:46:56 +0900

    init gui

--- source/kdr/fm6/client.d
+++ source/kdr/fm6/client.d
@@ -1,5 +1,6 @@
 module kdr.fm6.client;
 
+import core.stdc.stdio;
 import dplug.client;
 import dplug.core;
 import kdr.fm6.gui;
@@ -16,7 +17,17 @@ nothrow:
   }
 
   override Parameter[] buildParameters() {
-    return [];
+    Vec!Parameter ret;
+    int opIdx = 0;
+    foreach (x; 0 .. numOps) {
+      foreach (y; 0 .. numOps) {
+        char[] name = mallocSlice!char(10);
+        int len = sprintf(name.ptr, "op%d/%d", x, y);
+        ret.pushBack(mallocNew!LinearFloatParameter(opIdx, cast(string) name[0 .. len], "", -1, 1, 0));
+        ++opIdx;
+      }
+    }
+    return ret.releaseData;
   }
 
   @safe
@@ -49,4 +60,17 @@ nothrow:
 private:
   Fm6GUI _gui;
   double _sampleRate;
+}
+
+unittest {
+  auto client = new Fm6Client;
+  auto gui = new Fm6GUI(client.params);
+  gui.reflow();
+
+  int w = 100, h = 100;
+  auto dif = new OwnedImage!RGBA(w, h);
+  auto dep = new OwnedImage!L16(w, h);
+  auto mat = new OwnedImage!RGBA(w, h);
+  gui.onDrawPBR(toRef(dif), toRef(dep), toRef(mat), [rectangle(0, 0, w, h)]);
+  assert(false);
 }
--- source/kdr/fm6/gui.d
+++ source/kdr/fm6/gui.d
@@ -1,15 +1,77 @@
 module kdr.fm6.gui;
 
+import std.algorithm : min;
+
+import dplug.core;
 import dplug.client;
 import dplug.gui;
+import dplug.pbrwidgets;
 import kdr.simplegui : PBRSimpleGUI;
+
+enum RGBA lineColor = RGBA(0, 255, 255, 96);
+enum RGBA gradColor = RGBA(0, 64, 64, 96);
+enum RGBA gridColor = RGBA(100, 200, 200, 32);
+enum RGBA darkColor = RGBA(128, 128, 128, 128);
+enum RGBA lightColor = RGBA(100, 200, 200, 100);
+enum RGBA textColor = RGBA(155, 255, 255, 0);
+enum RGBA knobColor = RGBA(96, 96, 96, 96);
+enum RGBA litColor = RGBA(155, 255, 255, 0);
+enum RGBA unlitColor = RGBA(0, 32, 32, 0);
+
+enum int numOps = 6;
 
 ///
 class Fm6GUI : PBRSimpleGUI {
+public:
   ///
   @nogc nothrow
   this(Parameter[] params) {
     static immutable float[] ratios = [1.0f, 1.25f, 1.5f, 1.75f, 2.0f];
     super(makeSizeConstraintsDiscrete(600, 400, ratios));
+
+    this._params = params;
+
+    int opIdx = 0;
+    foreach (x; 0 .. numOps) {
+      foreach (y; 0 .. numOps) {
+        _opKnobs[opIdx] = buildKnob(params[opIdx]);
+        ++opIdx;
+      }
+    }
   }
+
+  override void reflow() {
+    super.reflow();
+    int W = this.position.width;
+    int H = this.position.height;
+    int knobSize = min(W, H) / numOps;
+    int opIdx = 0;
+    foreach (x; 0 .. numOps) {
+      foreach (y; 0 .. numOps) {
+        _opKnobs[opIdx].position = rectangle(x * knobSize, y * knobSize, knobSize, knobSize);
+        ++opIdx;
+      }
+    }
+  }
+
+private:
+@nogc nothrow:
+
+  UIKnob buildKnob(Parameter p) {
+    UIKnob knob;
+    addChild(knob = mallocNew!UIKnob(this.context, p));
+    knob.knobRadius = 0.65f;
+    knob.knobDiffuse = knobColor;
+    // NOTE: material [R(smooth), G(metal), B(shiny), A(phisycal)]
+    knob.knobMaterial = RGBA(255, 0, 0, 0);
+    knob.numLEDs = 0;
+    knob.litTrailDiffuse = litColor;
+    knob.unlitTrailDiffuse = unlitColor;
+    knob.trailRadiusMin = 0.1;
+    knob.trailRadiusMax = 0.8;
+    return knob;
+  }
+
+  Parameter[] _params;
+  UIKnob[numOps * numOps] _opKnobs;
 }
